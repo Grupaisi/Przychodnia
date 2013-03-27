01345c491d5fad55bfb99112d2cc219af8ea413a
Paweł Sobieraj <Pawciaczek@gmail.com>
2013-03-27 12:58:20 +0100

    index + css

---
+++ index.php
@@ -0,0 +1,58 @@
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+     <head>
+          <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>          
+          <meta content="text/html; charset=utf-8" http-equiv="Content-Type" />
+          <link rel="stylesheet" type="text/css" href="style.css" />
+          <script type="text/javascript" src="javascripts/zwijanie.js"></script>
+          <title>Klinika NFZ</title>
+     </head>
+
+     <body>
+          <div id="wrapper">
+               <div id="top">
+                    <div id="akg_med">
+                         
+                         
+                    </div>
+                    
+                    
+               </div>
+               <div id="container">
+                    <div id="left_column">                         
+                         
+
+                              <ul class="menu">
+                                   
+                                        <li><a href="index.php?id=RejestracjaPacjentow">Dodaj Pacjenta</a></li>
+                                        <li><a href="index.php?id=RejestracjaLekarzy">Dodaj Lekarza</a></li>
+                                        
+                         
+                    </div>
+
+                    <div id="right_column">
+                         
+                              <div class="content">
+                                  
+                                   <?php include("includes/list.php") ?>
+
+                              </div>
+
+
+
+                         </div>
+                         
+
+                    </div>
+
+               </div>
+
+
+
+
+          </div>
+     </body>
+
+</html>
