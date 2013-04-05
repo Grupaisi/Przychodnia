0a01fac0ee2b608c30b014a44e730aa9d584bfac
Grzegorz Sobieraj <grzesslav@gmail.com>
2013-04-05 09:12:57 +0200

    Formularze

---
+++ forms/RejestracjaLekarzy.php
@@ -0,0 +1,21 @@
+
+
+     <form method="POST" action="index.php?id=Rejestracja_Lekarzy">            
+          <table width="360px">
+               <tr><td>          Imię:                        <input type="text"         id="form_register"   name="imie" value="" /></td></tr>
+               <tr><td>          Nazwisko:                    <input type="text"         id="form_register"   name="nazwisko" value="" /></td></tr>
+               <tr><td>          Pesel:                       <input type="text"         id="form_register"   name="pesel" value="" /></td></tr>
+               <tr><td>          Hasło:                       <input type="password"     id="form_register"   name="password" value="" /></td></tr>
+               <tr><td>          Potwierdz hasło:             <input type="password"     id="form_register"   name="password_conf" value="" /></td></tr>
+               <tr><td>          Numer Licencji:              <input type="text"         id="form_register"   name="nr_licencji" value="" /></td></tr>
+               <tr><td>          Specjalizacja:               <input type="text"         id="form_register"   name="specjalizacja" value="" /></td></tr>
+               <tr><td>          Email:                       <input type="text"         id="form_register"   name="email" value="" /></td></tr>
+               <tr><td>          Numer Dowodu:                <input type="text"         id="form_register"   name="nr_dowodu" value="" /></td></tr>
+               <tr><td>          Ulica:                       <input type="text"         id="form_register"   name="ulica" value="" /></td></tr>
+               <tr><td>          Numer Domu:                  <input type="text"         id="form_register"   name="nr_domu" value="" /></td></tr>
+               <tr><td>          Numer Mieszkania:            <input type="text"         id="form_register"   name="nr_mieszkania" value="" /></td></tr>
+               <tr><td>          Kod Pocztowy:                <input type="text"         id="form_register"   name="kod_pocztowy" value="" /></td></tr>
+               <tr><td>          Numer Telefonu:              <input type="text"         id="form_register"   name="nr_tel" value="" /></td></tr>              
+          </table>
+          <input type="image" name="submit" value="Wyslij" src="images/zatwierdz.png" />
+
---
+++ forms/RejestracjaPacjentow.php
@@ -0,0 +1,23 @@
+
+
+
+<form method="POST" action="index.php?id=Rejestracja_Pacjentow">  
+     <table width="360px">
+          <tr><td>         Imię:                        <input type="text"        id="form_register"   name="imie" value="" /></td></tr>
+          <tr><td>         Nazwisko:                    <input type="text"        id="form_register"   name="nazwisko" value="" /></td></tr>
+          <tr><td>         Pesel:                       <input type="text"        id="form_register"   name="pesel" value="" /></td></tr>
+          <tr><td>         Hasło:                       <input type="password"    id="form_register"   name="password" value="" /></td></tr>
+          <tr><td>         Potwierdz hasło:             <input type="password"    id="form_register"   name="password_conf" value="" /></td></tr>
+          <tr><td>         Numer Ubezpieczenia:         <input type="text"        id="form_register"   name="nr_ubezpieczenia" value="" /></td></tr>
+          <tr><td>         Email:                       <input type="text"        id="form_register"   name="email" value="" /></td></tr>
+          <tr><td>         Numer Dowodu:                <input type="text"        id="form_register"   name="nr_dowodu" value="" /></td></tr>
+          <tr><td>         Ulica:                       <input type="text"        id="form_register"   name="ulica" value="" /></td></tr>
+          <tr><td>         Numer Domu:                  <input type="text"        id="form_register"   name="nr_domu" value="" /></td></tr>
+          <tr><td>         Numer Mieszkania:            <input type="text"        id="form_register"   name="nr_mieszkania" value="" /></td></tr>
+          <tr><td>         Kod Pocztowy:                <input type="text"        id="form_register"   name="kod_pocztowy" value="" /></td></tr>
+          <tr><td>         Miasto:                      <input type="text"        id="form_register"   name="miasto" value="" /></td></tr>         
+          <tr><td>         Numer Telefonu:              <input type="text"        id="form_register"   name="nr_tel" value="" /></td></tr>             
+     </table>
+     <input type="image" name="submit" value="Wyslij" src="images/zatwierdz.png" />
+</form>
+
